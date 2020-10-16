2e283ebfdc0cd02fa97e7c3af068f990ce5aa59b
Saravanan raghul <saravananraghul1820@gmail.com>
2020-10-16 22:56:59 +0530

    getting input from form

--- app.ts
+++ app.ts
@@ -1,9 +1,18 @@
+const num1Ele = document.getElementById('n1') as HTMLInputElement;
+const num2Ele = document.getElementById('n2') as HTMLInputElement;
+const buttonEle = document.querySelector('button');
+
 // General Check
 function add(num1: number, num2: number){
     return num1+num2;
 }
 
-console.log(add(1,5));
+buttonEle.addEventListener('click',() => {
+    const n1 = num1Ele.value;
+    const n2 = num2Ele.value;
+    const result = add(+n1, +n2);
+});
+// console.log(add(1,5));
 // console.log(add('1','5')); -> String
 
 
