2d3aedebcc7d6be9c3a5df0a71ef5a73bb35d767
Saravanan raghul <saravananraghul1820@gmail.com>
2020-10-17 19:50:11 +0530

    Union Type

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ var num2Ele = document.getElementById('n2');
 var buttonEle = document.querySelector('button'); // -> adding '!' bcoz of null type err in ts
 // General Check
 function add(n1, n2) {
-    return n1 + n2;
+    if (typeof n1 === 'number' && typeof n2 === 'number')
+        return n1 + n2;
+    else if (typeof n1 === 'string' && typeof n2 === 'string')
+        return n1 + " " + n2;
+    else
+        return +n1 + +n2;
 }
 buttonEle.addEventListener('click', function () {
     var n1 = num1Ele.value;
     var n2 = num2Ele.value;
-    var result = add(+n1, +n2); //using + since .value is string we r forcing to accept number through '+' 
+    var result = add(n1, n2); //using + since .value is string we r forcing to accept number through '+' 
+    var StringResult = add(n1, n2);
     console.log(result);
+    console.log(StringResult);
 });
 // console.log(add(1,5));
 // console.log(add('1','5')); -> String
--- app.ts
+++ app.ts
@@ -4,15 +4,22 @@ const num2Ele = document.getElementById('n2') as HTMLInputElement;
 const buttonEle = document.querySelector('button')!; // -> adding '!' bcoz of null type err in ts
 
 // General Check
-function add(n1: number, n2: number){
-    return n1+n2;
+function add(n1: number | string, n2: number | string){
+    if(typeof n1 === 'number' && typeof n2 === 'number')
+        return n1+n2;
+    else if(typeof  n1 === 'string' && typeof  n2 === 'string')
+        return n1+ " " +n2;
+    else
+        return +n1 + +n2;
 }
 
 buttonEle.addEventListener('click',() => {
     const n1 = num1Ele.value;
     const n2 = num2Ele.value;
     const result = add(+n1, +n2);   //using + since .value is string we r forcing to accept number through '+' 
+    const StringResult = add(n1, n2);
     console.log(result);
+    console.log(StringResult);
 });
 // console.log(add(1,5));
 // console.log(add('1','5')); -> String
